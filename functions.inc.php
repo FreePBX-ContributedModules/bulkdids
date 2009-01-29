c6ac015c931465c26e241190ff366e18ceb291d9
Ethan Schroeder <ethan.schroeder@gmail.com>
2009-01-29 19:23:00 +0000

    First test of bulk did module

--- functions.inc.php
+++ functions.inc.php
@@ -1,225 +1,28 @@
 <?php
-/* functions.inc.php - functions for BulkExtensions module. */
-include_once("modules/voicemail/functions.inc.php");		// for using voicemail module functions to retrieve voicemail settings
-include_once("modules/dictate/functions.inc.php");		// for using dictation services functions to retrieve dictation settings
-include_once("modules/languages/functions.inc.php");		// for using languages functions to retrieve language setting
-include_once("modules/findmefollow/functions.inc.php");	// for using findmefollow functions to retreive follow me settings
+/* functions.inc.php - functions for BulkDIDs module. */
 
-/* Verify existence of voicemail, dictate, languages and findmefollow functions. */
-if (function_exists("voicemail_mailbox_get") && function_exists("voicemail_mailbox_add") && function_exists("voicemail_mailbox_del") && function_exists("voicemail_mailbox_remove") && class_exists("vmxObject")) {
-	$vm_exists	= TRUE;
-} else {
-	$vm_exists	= FALSE;
-}
-if (function_exists("dictate_get") && function_exists("dictate_update") && function_exists("dictate_del")) {
-	$dict_exists	= TRUE;
-} else {
-	$dict_exists	= FALSE;
-}
-if (function_exists("languages_user_get") && function_exists("languages_user_update") && function_exists("languages_user_del")) {
-	$lang_exists	= TRUE;
-} else {
-	$lang_exists	= FALSE;
-}
-if (function_exists("findmefollow_get") && function_exists("findmefollow_add") && function_exists("findmefollow_del")) {
-	$findme_exists	= TRUE;
-} else {
-	$findme_exists	= FALSE;
-}
-
-function exportextensions_allusers() {
+function exportdids_all() {
 	global $db;
-	global $vm_exists;
-	global $dict_exists;
-	global $lang_exists;
-	global $findme_exists;
 	$action		= "edit";
-	$fname		= "bulkext__" .  (string) time() . $_SERVER["SERVER_NAME"] . ".csv";
-	$csv_header 	= "action,extension,name,cid_masquerade,sipname,outboundcid,ringtimer,callwaiting,call_screen,password,emergency_cid,tech,hardware,devinfo_channel,devinfo_secret,devinfo_notransfer,devinfo_dtmfmode,devinfo_canreinvite,devinfo_context,devinfo_immediate,devinfo_signalling,devinfo_echocancel,devinfo_echocancelwhenbrdiged,devinfo_echotraining,devinfo_busydetect,devinfo_busycount,devinfo_callprogress,devinfo_host,devinfo_type,devinfo_nat,devinfo_port,devinfo_qualify,devinfo_callgroup,devinfo_pickupgroup,devinfo_disallow,devinfo_allow,devinfo_dial,devinfo_accoountcode,devinfo_mailbox,devicetype,deviceid,deviceuser,description,dictenabled,dictformat,dictemail,langcode,record_in,record_out,vm,vmpwd,email,pager,attach,saycid,envelope,delete,options,vmcontext,vmx_state,vmx_unavail_enabled,vmx_busy_enabled,vmx_play_instructions,vmx_option_0_sytem_default,vmx_option_0_number,vmx_option_1_system_default,vmx_option_1_number,vmx_option_2_number,account,ddial,pre_ring,strategy,grptime,grplist,annmsg_id,ringing,grppre,dring,needsconf,remotealert_id,toolate_id,postdest\n";
+	$fname		= "bulkdids__" .  (string) time() . $_SERVER["SERVER_NAME"] . ".csv";
+	$csv_header 	= "action,DID,description,destination,cidnum,alertinfo,grppre,mohclass,ringing,privacyman\n";
 	$data 		= $csv_header;
-	$exts 		= get_all_exts();
+	$exts 		= get_all_dids();
 
 	foreach ($exts as $ext) {
 		$e 	= $ext[0];
-		$u_info = core_users_get($e);
-		$d_info = core_devices_get($e);
-		if ($vm_exists) {
-			$v_info	= voicemail_mailbox_get($e);
-		} else {
-			$v_info = NULL;
-		}
-		/* To properly obtain voicemail information, detect enabled/disabled vm value.   */
-		/* Parse extra voicemail options.						 */
-		if ($v_info == NULL) {
-			$v_enabled	= "disabled";
-		} else {
-			$v_enabled 	= "enabled";
-			$v_options 	= isset($v_info["options"])?$v_info["options"]:"";
-			$vm_other_opts 	= "";
-			$i 		= 0;
-			$first 		= TRUE;
-			$c 		= count($v_options);
-			reset($v_options);
-			while ($i < $c) {
-				if ((key($v_options) != "attach") && (key($v_options) != "saycid") && (key($v_options) != "envelope") && (key($v_options) != "delete")) {
-					if ($first) {
-						$vm_other_opts	= key($v_options) . "=" . $v_options[key($v_options)];
-						$first 		= false;
-					} else {
-						$vm_other_opts .=  "|" . key($v_options) . "=" . $v_options[key($v_options)];
-					}
-				}
-				$i++;
-				next($v_options);
-			}
-		}
-		/* Obtain vmx settings. */
-		if ($vm_exists) {
-			$vmxobj		= new vmxObject($e);
-		} else {
-			$vmxobj		= NULL;
-		}
-		
-		if (is_object($vmxobj)) {
-			$vmx_state 		= ($vmxobj->isEnabled())?"checked":"";
-			$vmx_unavail_enabled 	= ($vmxobj->getState("unavail")=="enabled")?"checked":"";
-			$vmx_busy_enabled 	= ($vmxobj->getState("busy")=="enabled")?"checked":"";
-			$vmx_play_instructions 	= ($vmxobj->getVmPlay())?"checked":"";
-			$vmx_option_0_number 	= $vmxobj->getMenuOpt(0);
-			if ($vmx_option_0_number == "") {
-				$vmx_option_0_system_default = "checked";
-			} else {
-				$vmx_option_0_system_default = "";
-			}
-			if (is_object($vmxobj)) {
-				if ($vmxobj->hasFollowMe() && $vmxobj->isFollowMe()) {
-					$vmx_option_1_system_default 	= "checked";
-					$vmx_option_1_number 		= "";
-				} else {
-					$vmx_option_1_system_default 	= "";
-					$vmx_option_1_number 		= $vmxobj->getMenuOpt(1);
-				}
-				$vmx_option_2_number 			= $vmxobj->getMenuOpt(2);
-			}
-		}
-			
-		/* Obtain dictation services settings. */
-		if ($dict_exists) {
-			$dictate_settings = dictate_get($e);
-		}
-
-		/* Obtain language code. */
-		if ($lang_exists) {
-			$langcode = languages_user_get($e);
-		}
-
-		/* Obtain follow me settings. */
-		if ($findme_exists) {
-			$followme_settings = findmefollow_get($u_info["extension"], TRUE);
-		}
-		if (isset($followme_settings)) {
-			$account	= isset($followme_settings["grpnum"])?$followme_settings["grpnum"]:"";
-			$strategy	= isset($followme_settings["strategy"])?$followme_settings["strategy"]:"";
-			$grptime	= isset($followme_settings["grptime"])?$followme_settings["grptime"]:"";
-			$grppre		= isset($followme_settings["grppre"])?$followme_settings["grppre"]:"";
-			$grplist	= isset($followme_settings["grplist"])?$followme_settings["grplist"]:"";
-			$annmsg_id	= isset($followme_settings["annmsg_id"])?$followme_settings["annmsg_id"]:"";
-			$postdest	= isset($followme_settings["postdest"])?$followme_settings["postdest"]:"";
-			$dring 		= isset($followme_settings["dring"])?$followme_settings["dring"]:"";
-			$needsconf 	= isset($followme_settings["needsconf"])?$followme_settings["needsconf"]:"";
-			$remotealert_id = isset($followme_settings["remotealert_id"])?$followme_settings["remotealert_id"]:"";
-			$toolate_id 	= isset($followme_settings["toolate_id"])?$followme_settings["toolate_id"]:"";
-			$ringing 	= isset($followme_settings["ringing"])?$followme_settings["ringing"]:"";
-			$pre_ring 	= isset($followme_settings["pre_ring"])?$followme_settings["pre_ring"]:"";
-			$ddial 		= isset($followme_settings["ddial"])?$followme_settings["ddial"]:"";
-		}
+		$did_info = core_dids_get($e['extension',$e['cidnum']);
 		$csv_line[0] 	= $action;
-		$csv_line[1] 	= isset($u_info["extension"])?$u_info["extension"]:"";
-		$csv_line[2] 	= isset($u_info["name"])?$u_info["name"]:"";
-		$csv_line[3] 	= isset($u_info["cid_masquerade"])?$u_info["cid_masquerade"]:"";
-		$csv_line[4] 	= isset($u_info["sipname"])?$u_info["sipname"]:"";
-		$csv_line[5] 	= isset($u_info["outboundcid"])?$u_info["outboundcid"]:"";
-		$csv_line[6] 	= isset($u_info["ringtimer"])?$u_info["ringtimer"]:"";
-		$csv_line[7]	= isset($u_info["callwaiting"])?$u_info["callwaiting"]:"";
-		$csv_line[8]	= isset($u_info["call_screen"])?$u_info["call_screen"]:"0";
-		$csv_line[9]	= isset($u_info["password"])?$u_info["password"]:"";
+		$csv_line[1] 	= isset($did_info["extension"])?$did_info["extension"]:"";
+		$csv_line[2] 	= isset($did_info["description"])?$did_info["description"]:"";
+		$csv_line[3] 	= isset($did_info["destination"])?$did_info["cid_destination"]:"";
+		$csv_line[4] 	= isset($did_info["cidnum"])?$did_info["cidnum"]:"";
+		$csv_line[5] 	= isset($did_info["alertinfo"])?$did_info["alertinfo"]:"";
+		$csv_line[6] 	= isset($did_info["grppre"])?$did_info["grppre"]:"";
+		$csv_line[7]	= isset($did_info["mohclass"])?$did_info["mohclass"]:"";
+		$csv_line[8]	= isset($did_info["ringing"])?$did_info["ringing"]:"0";
+		$csv_line[9]	= isset($did_info["privacyman"])?$did_info["privacyman"]:"";
 
-		$csv_line[10]	= isset($d_info["emergency_cid"])?$d_info["emergency_cid"]:"";
-		$csv_line[11]	= isset($d_info["tech"])?$d_info["tech"]:"";
-		$csv_line[12]	= ""; 	// hardware
-		$csv_line[13]	= isset($d_info["channel"])?$d_info["channel"]:"";
-		$csv_line[14]	= isset($d_info["secret"])?$d_info["secret"]:"";
-		$csv_line[15]	= isset($d_info["notransfer"])?$d_info["notransfer"]:"";
-		$csv_line[16]	= isset($d_info["dtmfmode"])?$d_info["dtmfmode"]:"";
-		$csv_line[17]	= isset($d_info["canreinvite"])?$d_info["canreinvite"]:"";
-		$csv_line[18]	= isset($d_info["context"])?$d_info["context"]:"";
-		$csv_line[19]	= isset($d_info["immediate"])?$d_info["immediate"]:"";
-		$csv_line[20]	= isset($d_info["signalling"])?$d_info["signalling"]:"";
-		$csv_line[21]	= isset($d_info["echocancel"])?$d_info["echocancel"]:"";
-		$csv_line[22]	= isset($d_info["echocancelwhenbridged"])?$d_info["echocancelwhenbridged"]:"";
-		$csv_line[23]	= isset($d_info["echotraining"])?$d_info["echotraining"]:"";
-		$csv_line[24]	= isset($d_info["busydetect"])?$d_info["busydetect"]:"";
-		$csv_line[25]	= isset($d_info["busycount"])?$d_info["busycount"]:"";
-		$csv_line[26]	= isset($d_info["callprogress"])?$d_info["callprogress"]:"";
-		$csv_line[27]	= isset($d_info["host"])?$d_info["host"]:"";
-		$csv_line[28]	= isset($d_info["type"])?$d_info["type"]:"";
-		$csv_line[29]	= isset($d_info["nat"])?$d_info["nat"]:"";
-		$csv_line[30]	= isset($d_info["port"])?$d_info["port"]:"";
-		$csv_line[31]	= isset($d_info["qualify"])?$d_info["qualify"]:"";
-		$csv_line[32]	= isset($d_info["callgroup"])?$d_info["callgroup"]:"";
-		$csv_line[33]	= isset($d_info["pickupgroup"])?$d_info["pickupgroup"]:"";
-		$csv_line[34]	= isset($d_info["disallow"])?$d_info["disallow"]:"";
-		$csv_line[35]	= isset($d_info["allow"])?$d_info["allow"]:"";
-		$csv_line[36]	= isset($d_info["dial"])?$d_info["dial"]:"";
-		$csv_line[37]	= isset($d_info["accountcode"])?$d_info["accountcode"]:"";
-		$csv_line[38]	= isset($d_info["mailbox"])?$d_info["mailbox"]:"";
-		$csv_line[39]	= isset($d_info["devicetype"])?$d_info["devicetype"]:"fixed";
-		$csv_line[40]	= (isset($d_info["deviceid"]) || ($d_info["deviceid"]==""))?$d_info["deviceid"]:(isset($u_info["extension"])?$u_info["extension"]:"");
-		$csv_line[41]	= (isset($d_info["deviceuser"]) && ($d_info["deviceuser"] != ""))?$d_info["deviceuser"]:(isset($u_info["extension"])?$u_info["extension"]:"none");
-		$csv_line[42]	= isset($d_info["description"])?$d_info["description"]:(isset($u_info["name"])?$u_info["name"]:"");
-
-		$csv_line[43]	= isset($dictate_settings["enabled"])?$dictate_settings["enabled"]:"disabled";	// dictenabled
-		$csv_line[44]	= isset($dictate_settings["format"])?$dictate_settings["format"]:"ogg";		// dictformat (ogg is default)
-		$csv_line[45]	= isset($dictate_settings["email"])?$dictate_settings["email"]:""; 		// dictemail
-
-		$csv_line[46]	= isset($langcode)?$langcode:"";
-
-		$csv_line[47]	= isset($d_info["record_in"])?$d_info["record_in"]:"";
-		$csv_line[48]	= isset($d_info["record_out"])?$d_info["record_out"]:"";
-
-		$csv_line[49]	= $v_enabled; // vm
-		$csv_line[50]	= isset($v_info["pwd"])?$v_info["pwd"]:"";
-		$csv_line[51]	= isset($v_info["email"])?$v_info["email"]:"";
-		$csv_line[52]	= isset($v_info["pager"])?$v_info["pager"]:"";
-		$csv_line[53]	= isset($v_info["options"]["attach"])?("attach=" . $v_info["options"]["attach"]):"attach=no";
-		$csv_line[54]	= isset($v_info["options"]["saycid"])?("saycid=" . $v_info["options"]["saycid"]):"saycid=no";
-		$csv_line[55]	= isset($v_info["options"]["envelope"])?("envelope=" . $v_info["options"]["envelope"]):"envelope=no";
-		$csv_line[56]	= isset($v_info["options"]["delete"])?("delete=". $v_info["options"]["delete"]):"delete=no";
-		$csv_line[57]	= isset($vm_other_opts)?$vm_other_opts:""; // additional options
-		$csv_line[58]	= isset($v_info["vmcontext"])?$v_info["vmcontext"]:"";
-		$csv_line[59]	= isset($vmx_state)?$vmx_state:"";
-		$csv_line[60]	= isset($vmx_unavail_enabled)?$vmx_unavail_enabled:"";
-		$csv_line[61]	= isset($vmx_busy_enabled)?$vmx_busy_enabled:"";
-		$csv_line[62]	= isset($vmx_play_instructions)?$vmx_play_instructions:"";
-		$csv_line[63]	= isset($vmx_option_0_system_default)?$vmx_option_0_system_default:"";
-		$csv_line[64]	= isset($vmx_option_0_number)?$vmx_option_0_number:"";
-		$csv_line[65]	= isset($vmx_option_1_system_default)?$vmx_option_1_system_default:"";
-		$csv_line[66]	= isset($vmx_option_1_number)?$vmx_option_1_number:"";
-		$csv_line[67]	= isset($vmx_option_2_number)?$vmx_option_2_number:"";
-		$csv_line[68]	= isset($account)?$account:"";
-		$csv_line[69]	= isset($ddial)?$ddial:"";
-		$csv_line[70]	= isset($pre_ring)?$pre_ring:"";
-		$csv_line[71]	= isset($strategy)?$strategy:"";
-		$csv_line[72]	= isset($grptime)?$grptime:"";
-		$csv_line[73]	= isset($grplist)?$grplist:"";
-		$csv_line[74]	= isset($annmsg_id)?$annmsg_id:"";
-		$csv_line[75]	= isset($ringing)?$ringing:"";
-		$csv_line[76]	= isset($grppre)?$grppre:"";
-		$csv_line[77]	= isset($dring)?$dring:"";
-		$csv_line[78]	= isset($needsconf)?$needsconf:"";
-		$csv_line[79]	= isset($remotealert_id)?$remotealert_id:"";
-		$csv_line[80]	= isset($toolate_id)?$toolate_id:"";
-		$csv_line[81]	= isset($postdest)?$postdest:"";
 		for ($i = 0; $i < count($csv_line); $i++) {
 			/* If the string contains a comma, enclose it in double-quotes. */
 			if (strpos($csv_line[$i], ",") !== FALSE) {
@@ -238,8 +41,8 @@ function exportextensions_allusers() {
 	return;
 }
 
-function get_all_exts() {
-	$sql 	= "SELECT extension FROM users ORDER BY extension";
+function get_all_dids() {
+	$sql 	= "SELECT extension,cidnum FROM incoming ORDER BY extension";
 	$extens = sql($sql,"getAll");
 	if (isset($extens)) {
 		return $extens;
@@ -283,15 +86,14 @@ function ob_clean_all () {
 }
 
 function generate_table_rows() {
-	$fh = fopen("modules/bulkextensions/table.csv", "r");
+	$fh = fopen("modules/bulkdids/table.csv", "r");
 	if ($fh == NULL) {
 		return NULL;
 	}
 	$k = 0;
 	while (($csv_data = fgetcsv($fh, 1000, ",", "\"")) !== FALSE) {
 		$k++;
-		/* Name,Default,Allowed,On Extensions page,Details */
-		for ($i = 0; $i < 5; $i++) {
+		for ($i = 0; $i < 4; $i++) {
 			if (isset($csv_data[$i])) {
 				$table[$k][$i] .= $csv_data[$i];
 			} else {
---
+++ page.bulkdids.php
@@ -0,0 +1,210 @@
+<?php
+set_time_limit(3000);
+
+function fatal($text)  {
+	$clean = str_replace("</script>","",str_replace("<script>javascript:alert('","",$text));
+	return "\t".$clean."\n";
+}
+
+// $change is used as a flag whether or not a reload is needed. If no changes
+// are made, no reload will be prompted.
+$change = false;
+$output = "";
+
+if ($_REQUEST["csv_type"] == "output") {
+	exportdids_all();
+} elseif ($_REQUEST["csv_type"] == "input") {
+
+    $aFields = array (
+      "action" => array(false, -1),
+      "DID" => array(false, -1),
+      "description" => array(false, -1),
+      "destination" => array(false, -1),
+      "cidnum" => array(false, -1),
+      "alertinfo" => array(false, -1),
+      "grppre" => array(false, -1),
+      "mohclass" => array(false, -1),
+      "ringing" => array(false, -1),
+      "privacyman" => array(false, -1)
+      );
+
+      $fh = fopen($_FILES["csvFile"]["tmp_name"], "r");
+      if ($fh == NULL) {
+	      $file_ok = FALSE;
+      } else {
+	      $file_ok = TRUE;
+      }
+
+      $k = 0;
+
+      while ($file_ok && (($aInfo = fgetcsv($fh, 2000, ",", "\"")) !== FALSE)) {
+              $k++;
+	      if (empty($aInfo[0])) {
+		      continue;
+	      }
+
+	      // If this is the first row then we need to check each field listed (these are the headings)
+	      if ($i==0) {
+		      for ($j=0; $j<count($aInfo); $j++) {
+			      $aKeys = array_keys($aFields);
+			      foreach ($aKeys as $sKey) {
+				      if ($aInfo[$j] == $sKey) {
+					      $aFields[$sKey][0] = true;
+					      $aFields[$sKey][1] = $j;
+				      }
+			      }
+		      }
+		      $i++;
+		      $output .= "<BR><BR>Row $k: Headers parsed. <BR>";
+		      continue;
+	      }
+
+	      if ($aFields["action"][0]) {
+		      $vars["action"] = trim($aInfo[$aFields["action"][1]]);
+	      }
+
+	      if ($aFields["did"][0]) {
+		      $vars["extension"]  = trim($aInfo[$aFields["DID"][1]]);
+	      }
+
+	      if ($aFields["description"][0]) {
+		      $vars["description"] = trim($aInfo[$aFields["description"][1]]);
+	      }
+
+	      if ($aFields["destination"][0]) {
+		      $vars["destination"] = trim($aInfo[$aFields["destination"][1]]);
+	      }
+
+	      if ($aFields["cidnum"][0]) {
+		      $vars["cidnum"] = trim($aInfo[$aFields["cidnum"][1]]);
+	      }
+
+	      if ($aFields["alertinfo"][0]) {
+		      $vars["alertinfo"] = trim($aInfo[$aFields["alertinfo"][1]]);
+	      }
+
+	      if ($aFields["grppre"][0]) {
+		      $vars["grppre"] = trim($aInfo[$aFields["grppre"][1]]);
+	      }
+
+      	      if ($aFields["mohclass"][0]) {
+		      $vars["mohclass"] = trim($aInfo[$aFields["mohclass"][1]]);
+	      }
+
+	      if ($aFields["ringing"][0]) {
+		      $vars["ringing"] = trim($aInfo[$aFields["ringing"][1]]);
+	      }
+
+	      if ($aFields["privacyman"][0]) {
+		      $vars["privacyman"] = trim($aInfo[$aFields["privacyman"][1]]);
+	      }
+
+	      $vars["display"]	= "bulkdids";
+	      $vars["type"]	= "tool";
+
+	      $_REQUEST = $vars;
+
+	      if (checkRange($vars["extension"])) {
+
+		      switch ($vars["action"]) {
+		      	case "add":
+				ob_start("fatal");
+				core_did_add($vars);
+				ob_flush();
+
+				// begin status output for this row
+				$output .= "Row $k: Added: " . $vars["extension"];
+				$output .= "<br />";
+				$change = true;
+				break;
+			case "edit":
+				if (core_did_get($vars["extension"])) {
+					core_did_del($vars["extension"]);
+					$change = true;
+				}
+				$output .= "Row $k: Edited: " . $vars["extension"] . "<BR>";
+				break;
+			case "del":
+				if (core_did_get($vars["extension"])) {
+					core_did_del($vars["extension"]);
+					$change = true;
+				}
+				$output .= "Row $k: Deleted: " . $vars["extension"] . "<BR>";
+				break;
+			default:
+				$output .= "Row $k: Unrecognized action: the only actions recognized are add, edit, del.\n";
+				break;
+		      }
+	
+		      if ($change) {
+			  needreload();
+		      }
+	      } else {
+		      $output .= "Row $k: Access denied to did " . $vars["extension"] . ".  No action performed.<BR>";
+	      }
+      } // while loop
+
+       print $output;
+
+} else
+{
+	$table_output = "";
+	$table_rows = generate_table_rows();
+	if ($table_rows === NULL) {
+		$table_output = "Table unavailable";
+	} else {
+		$table_output .=	"<table cellspacing='0' cellpadding='4' rules='rows'>";
+		$table_output .=	"<tr valign='top'>
+						<th align='left' valign='top'>#</th>
+						<th align='left' valign='top'>DID</th>
+						<th align='left' valign='top'>Description</th>
+						<th align='left' valign='top'>Destination</th>
+						<th align='left' valign='top'>CID Num Match</th>
+					</tr>";
+		$i = 1;
+		foreach ($table_rows as $row) {
+			$table_output .= "<tr>";
+			$table_output .= "<td valign='top'>" . $i . "</td>";
+			$i++;
+			foreach ($row as $col) {
+				$table_output .= "<td valign='top'>" . $col . "</td>";
+			}
+			$table_output .= "</tr>";
+		}
+		$table_output .= "</table>";
+	}
+
+?>
+<h1>Bulk DIDs</h1>
+
+<h2>Manage DIDs in bulk using CSV files.</h2>
+
+<p>
+Start by downloading the
+<a href="modules/bulkdids/template.csv">Template CSV file</a>
+(right-click > save as) or clicking the Export DIDs button.
+</p>
+<p>
+Modify the CSV file to add, edit, or delete DIDs as desired. Then load
+the CSV file. After the CSV file is processed, the action taken for each row
+will be displayed.
+</p>
+<p>
+
+<form action="<?php $_SERVER["PHP_SELF"] ?>" name="uploadcsv" method="post" enctype="multipart/form-data">
+<input id="csv_type" name="csv_type" type="hidden" value="none" />
+<input type="submit" onclick="document.getElementById('csv_type').value='output';" value="Export DIDs" />
+&nbsp;&nbsp;CSV File to Load: <input name="csvFile" type="file" />
+<input type="submit" onclick="document.getElementById('csv_type').value='input';"  value="Load File" />
+<hr />
+</form>
+<hr />
+<h3>Bulk DIDs CSV File Columns</h3>
+<p>
+The table below explains each column in the CSV file. You can change the column
+order of the CSV file as you like, however, the column names must be preserved.
+</p>
+<?php
+	print $table_output;
+}
+?>
